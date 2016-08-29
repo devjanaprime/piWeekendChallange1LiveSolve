4834d921a565255904ef374c7caf4d6bfbd66937
devjanaprime <dev@primeacademy.io>
2016-08-29 11:25:22 -0500

    completed live solve

--- readme.md
+++ readme.md
@@ -2,3 +2,15 @@ Weekend Challenge 1 Live Solve for Pi
 =====================================
 
 http://i.imgur.com/nKgg9Vu.gifv
+
+* variable scope
+* encapsulate concerns into functions
+* function params/args
+* objects
+* arrays
+* for loops
+* splice
+* set the index of a button click from within a for loop
+* innerHTML
+* targeted a div by document.getElementById and saved this in a variable
+* refactored and adjusted for hoisting
--- scripts/salaries.js
+++ scripts/salaries.js
@@ -1,9 +1,10 @@
 console.log( 'salaries.js sourced' );
 // employees array
 var employees=[];
+var verbose = true;
 
 var addEmployee = function(){
-  console.log( 'in addEmployee' );
+  if( verbose ) console.log( 'in addEmployee' );
   // get user inputs and create a new employee object
   var newEmployee ={
     firstName: document.getElementById( 'firstNameIn' ).value,
@@ -12,10 +13,10 @@ var addEmployee = function(){
     jobTitle: document.getElementById( 'jobTitleIn' ).value,
     salary: document.getElementById( 'salaryIn' ).value
   }; // end new employee object
-  console.log( 'newEmployee', newEmployee );
+  if( verbose ) console.log( 'newEmployee', newEmployee );
   // push this employee in to the array
   employees.push( newEmployee );
-  console.log( 'employees:', employees );
+  if( verbose ) console.log( 'employees:', employees );
   // clear inputs
   clearInputs();
   /// - maybe move to another function? - ///
@@ -23,7 +24,7 @@ var addEmployee = function(){
 }; // end addEmployee
 
 var calculateSalaryInformation = function(){
-  console.log( 'in calculateSalaryInformation' );
+  if( verbose ) console.log( 'in calculateSalaryInformation' );
   // calculate total salary and avg monthly salary
   var totalSalaries = 0;
   // for each employee add their salary on to the totalSalaries
@@ -31,17 +32,17 @@ var calculateSalaryInformation = function(){
     // add this employee's salary to totalSalaries
     totalSalaries += Number( employees[i].salary );
   }
-  console.log( 'totalSalaries:', totalSalaries );
+  if( verbose ) console.log( 'totalSalaries:', totalSalaries );
   var averageSalary = totalSalaries / employees.length;
-  console.log( 'averageSalary:', averageSalary );
+  if( verbose ) console.log( 'averageSalary:', averageSalary );
   var monthlySalaryCost = totalSalaries / 12;
-  console.log( 'monthlySalaryCost:', monthlySalaryCost );
+  if( verbose ) console.log( 'monthlySalaryCost:', monthlySalaryCost );
   // displayEmployees
   displayEmployees( averageSalary, monthlySalaryCost, totalSalaries );
 }; // end calculateSalaryInformation
 
 var clearInputs = function(){
-  console.log( 'in clearInputs' );
+  if( verbose ) console.log( 'in clearInputs' );
   document.getElementById( 'firstNameIn' ).value = '';
   document.getElementById( 'lastNameIn' ).value = '';
   document.getElementById( 'employeeIdIn' ).value = '';
@@ -50,30 +51,32 @@ var clearInputs = function(){
 }; // end clearInputs
 
 var displayEmployees = function( avgSal, monthlySal, totalSal ){
-  console.log( 'in displayEmployees' );
+  if( verbose ) console.log( 'in displayEmployees' );
   // display info on DOM
   // clear output
   var outputDiv = document.getElementById( 'outputDiv' );
   outputDiv.innerHTML = '';
   // each employee
   for (var i = 0; i < employees.length; i++) {
-    var outputText = '<p>' + employees[i].lastName + ', ' + employees[i].firstName + ' (' + employees[i].employeeId + '): $' + employees[i].salary + '<button onclick="terminateEmployee( ' + i + ' );">Goodbye</button></p>';
+    var outputText = '<p>' + employees[i].lastName + ', ' + employees[i].firstName + ' (' + employees[i].employeeId + '): $' + Number( employees[i].salary ).toLocaleString( 'en-US', { minimumFractionDigits: 2, maximumFractionDigits: 2 } ) + '<button onclick="terminateEmployee( ' + i + ' );">Goodbye</button></p>';
     outputDiv.innerHTML += outputText;
   }
   // totalSalaries
-  outputText = '<p>Total salaries: $' + totalSal + '</p>';
+  outputText = '<p>Total salaries: $' + totalSal.toLocaleString( 'en-US', { minimumFractionDigits: 2, maximumFractionDigits: 2 } ) + '</p>';
   outputDiv.innerHTML += outputText;
   // averageSalary
-  outputText = '<p>Average Annual Salary: $' + avgSal + '</p>';
+  outputText = '<p>Average Annual Salary: $' + avgSal.toLocaleString( 'en-US', { minimumFractionDigits: 2, maximumFractionDigits: 2 } ) + '</p>';
   outputDiv.innerHTML += outputText;
   // monthlySalaryCost
-  outputText = '<p>Monthly Salary Cost: $' + monthlySal + '</p>';
+  outputText = '<p>Monthly Salary Cost: $' + monthlySal.toLocaleString( 'en-US', { minimumFractionDigits: 2, maximumFractionDigits: 2 } ) + '</p>';
   outputDiv.innerHTML += outputText;
 }; // end displayEmployees
 
 var terminateEmployee = function( index ){
-  console.log( 'in terminateEmployee:', index );
-  console.log( '(ex)terminating:', employees[ index ] );
+  if( verbose ){
+    console.log( 'in terminateEmployee:', index );
+    console.log( '(ex)terminating:', employees[ index ] );
+  } // end verbose
   // splice 1 from the employees array at our index
   employees.splice( index, 1 );
   // recalculate and update DOM
